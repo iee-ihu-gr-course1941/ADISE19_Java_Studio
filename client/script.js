7b67951930185ca8e9b00f336fa17aac1b2f22be
steliosntailakis <57562814+steliosntailakis@users.noreply.github.com>
2019-12-09 22:21:42 +0200

    added winning conditions

--- client/script.js
+++ client/script.js
@@ -3,6 +3,8 @@ const socket = io()
 let playerTurn = true
 let playerSymbol = "X"
 
+
+
 socket.on("startGame", (symbol) => {
     playerTurn = symbol === "X"
     playerSymbol = symbol
@@ -17,6 +19,24 @@ socket.on("updateGame", (symbol, button) => {
     $(`#${button}`).text(symbol)
     $(".game > button").attr("disabled", !playerTurn)
     $("#message").text(playerTurn ? "It's your turn." : "It's your opponent's turn.")
+
+    const combos = [
+        $("#00").text() + $("#01").text() + $("#02").text(),
+        $("#10").text() + $("#11").text() + $("#12").text(),
+        $("#20").text() + $("#21").text() + $("#22").text(),
+        $("#00").text() + $("#10").text() + $("#20").text(),
+        $("#01").text() + $("#11").text() + $("#21").text(),
+        $("#02").text() + $("#12").text() + $("#22").text(),
+        $("#00").text() + $("#11").text() + $("#22").text(),
+        $("#02").text() + $("#11").text() + $("#20").text()
+    ]
+    
+    for (let i = 0; i < combos.length; i++)
+    {
+        if (combos[i] === "XXX") alert("Player X has won!")
+        else if (combos[i] === "OOO") alert("Player O has won!")     
+    }
+
 })
 
 $(document).ready(() => {
